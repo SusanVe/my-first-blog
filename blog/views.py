c0833b7060ba700a18d6d7c368108f74fa8f8430
susanvew@hotmail.com <susanvew@hotmail.com>
2017-12-21 10:16:28 +0100

    rest api and minor fixes

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,18 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, SignUpForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from blog import views as blog_views
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -11,4 +13,7 @@ urlpatterns = [
     url(r'^signup/$', blog_views.signup, name='signup'),
     url(r'^login/$', auth_views.login,  name='login'),
     url(r'^logout/$', auth_views.logout,  name='logout'),
+    url(r'^blog/', blog_views.PostList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
